2dc5228c47af51eccba90651ce2de414dfd41d21
Anton Burkin <ramzzzay@gmail.com>
2020-01-16 13:07:11 +0300

    Init

---
+++ IntrinsicsTests/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IntrinsicsTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        
+        static unsafe public int[] Repeat(int value, int size)
+        {
+            int vecSize = 256 / 8 / 4;
+            int[] array = new int[size];
+
+            fixed (int* ptr = array)
+            {
+                
+            }
+            
+            
+            return new int[size];
+        }
+    }
+}
